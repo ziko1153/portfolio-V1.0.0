ca83c97ed537797e7118421e6a9d48566b3fe663
Tahmid Ziko <tahmidziko@gmail.com>
2020-08-22 14:20:15 +0600

    timeline design add

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach(content => {
+      content.classList.remove("active");
+    });
+    tabs.forEach(tab => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
